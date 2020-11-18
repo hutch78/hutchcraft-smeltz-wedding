9aab3ba1296baf21bcd53a74ede3717e573b1f06
Jeremy Hutchcraft <hutch78@me.com>
2020-11-18 15:30:00 -0500

    💌 update page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Allison Smeltz & Jeremy Hutchcraft are Getting Married',
+    title: '#HappilyEverHutchcraft',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
